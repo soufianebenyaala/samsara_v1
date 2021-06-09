eaf57c710c87e7f696f4fd658f1a8b80f6745161
Arbi <arbix91@gmail.com>
2021-06-09 16:49:30 +0100

    fetchProducts

--- samsara_v1/src/SearchPage/SearchResults/SearchResultCard/CardComponent/ImageContainer.js
+++ samsara_v1/src/SearchPage/SearchResults/SearchResultCard/CardComponent/ImageContainer.js
@@ -2,11 +2,11 @@ import React from 'react'
 import UseStyles from '../theme/index'
 import Chip from '@material-ui/core/Chip';
 
-const ImageContainer = () => {
+const ImageContainer = (props) => {
     const classes = UseStyles();
     return(
     <div className={classes.imgContainer}>
-      <div className={classes.img} style={{backgroundImage: 'url(https://img.zumpercdn.com/329255235/1280x960?auto=format&fit=crop&h=211&w=375&dpr=1), url(https://img.zumpercdn.com/329255235/1280x960?auto=format&fit=crop&h=11&w=20&dpr=1)',}}>
+      <div className={classes.img} style={{backgroundImage: `url(${props.image}), url(https://img.zumpercdn.com/329255235/1280x960?auto=format&fit=crop&h=11&w=20&dpr=1)`,}}>
         <div className={classes.badges}>
           <Chip style={{backgroundColor:"#911ea0",lineHeight: '9px',height:'15px',textTransform:'uppercase',color: '#fff',fontWeight: '700',padding: '3px 0px',fontSize: '10px',}}  label="featured"/>
         </div>
