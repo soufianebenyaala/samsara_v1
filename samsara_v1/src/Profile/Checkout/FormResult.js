9f764714c01ca836c9c53019783e2a1278c7a436
soufiane <soufianebenyaala1998@gmail.com>
2021-06-09 13:08:36 +0100

    add product complet

--- samsara_v1/src/Profile/Checkout/FormResult.js
+++ samsara_v1/src/Profile/Checkout/FormResult.js
@@ -1,46 +1,64 @@
-import React from 'react';
-import Typography from '@material-ui/core/Typography';
-import Grid from '@material-ui/core/Grid';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
+import React, { useState } from "react";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import DragAndDrop from "./DragAndDrop";
 
-export default function PaymentForm() {
+export default function PaymentForm(props) {
+
+
   return (
     <React.Fragment>
-      <Typography variant="h6" gutterBottom>
-        Payment method
-      </Typography>
+      <Typography variant="h6" gutterBottom></Typography>
       <Grid container spacing={3}>
-        <Grid item xs={12} md={6}>
-          <TextField required id="cardName" label="Name on card" fullWidth autoComplete="cc-name" />
+        <Grid item xs={12}>
+          <Typography variant="h6" component="h1">
+            put a document that validate your ownership of the proprety:
+          </Typography>
+
+          <DragAndDrop files={props.files} setFiles={props.setfiles} />
         </Grid>
-        <Grid item xs={12} md={6}>
+
+        <Grid item xs={12}>
+          <Typography variant="h6" component="h1">
+            put your images here:
+          </Typography>
+
+          <DragAndDrop files={props.images} setFiles={props.setimages} />
+        </Grid>
+
+        <Grid item xs={12}>
+          <Typography variant="h6" component="h1">
+            put the coordinates of your building at geographic coordinate system
+            :
+          </Typography>
+        </Grid>
+        <Grid item xs={12} sm={6}>
           <TextField
+            onChange={(e) => {
+              props.setlatitude(e.target.value);
+            }}
             required
-            id="cardNumber"
-            label="Card number"
-            fullWidth
-            autoComplete="cc-number"
-          />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <TextField required id="expDate" label="Expiry date" fullWidth autoComplete="cc-exp" />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <TextField
-            required
-            id="cvv"
-            label="CVV"
-            helperText="Last three digits on signature strip"
+            id="Latitude"
+            label="Latitude"
+            helperText="Latitude"
             fullWidth
             autoComplete="cc-csc"
           />
         </Grid>
-        <Grid item xs={12}>
-          <FormControlLabel
-            control={<Checkbox color="secondary" name="saveCard" value="yes" />}
-            label="Remember credit card details for next time"
+        <Grid item xs={12} sm={6}>
+          <TextField
+            onChange={(e) => {
+              props.setlongitude(e.target.value);
+            }}
+            required
+            id="Longitude"
+            label="Longitude"
+            helperText="Longitude"
+            fullWidth
+            autoComplete="cc-csc"
           />
         </Grid>
       </Grid>
