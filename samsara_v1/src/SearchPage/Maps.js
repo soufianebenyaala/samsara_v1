7d6f47510aa17ca01ee83d6465980d245bb4208d
soufiane <soufianebenyaala1998@gmail.com>
2021-06-23 16:01:05 +0100

    map fixe

--- samsara_v1/src/SearchPage/Maps.js
+++ samsara_v1/src/SearchPage/Maps.js
@@ -2,9 +2,35 @@ import * as React from 'react';
 import { useState } from 'react';
 import ReactMapGL, { Marker, Popup } from 'react-map-gl';
 import Room from '@material-ui/icons/Room';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 20,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
 
 function Map(props) {
+  const classes = useStyles();
+  const bull = <span className={classes.bullet}>•</span>;
+
   const [viewport, setViewport] = useState({
     latitude: 33.886917,
     longitude: 9.537499,
@@ -24,17 +50,19 @@ function Map(props) {
       mapStyle="mapbox://styles/soufiane0213/ckq539lxz0o8i18p3eep9obvt"
     >
       {props.blogs && props.blogs.map((blog) => (
-                <Marker 
-                id={blog.id}
-                key={blog.id} 
-                latitude={parseFloat(blog["data"].latitude)} 
-                longitude={parseFloat(blog["data"].longitude)} 
-                offsetLeft={-3.5 * viewport.zoom}
-                offsetTop={-7 * viewport.zoom}
-                >
-                <Room style={{ fontSize: viewport.zoom * 7, color: "red" }} />
-        {(props.IDbuilding == blog.id) ? (
-          
+        <>
+          <Marker
+            id={blog.id}
+            key={blog.id}
+            latitude={parseFloat(blog["data"].latitude)}
+            longitude={parseFloat(blog["data"].longitude)}
+            offsetLeft={-3.5 * viewport.zoom}
+            offsetTop={-7 * viewport.zoom}
+          >
+            <Room style={{ fontSize: viewport.zoom * 7, color: "red" }} />
+          </Marker>
+          {(props.IDbuilding == blog.id) ? (
+
             <Popup
               key={blog.id}
               latitude={parseFloat(blog["data"].latitude)}
@@ -42,24 +70,28 @@ function Map(props) {
               closeButton={true}
               closeOnClick={false}
               anchor="left" >
-              <div className="card">
-                <label>building Name</label>
-                <h4 className="place">{blog["data"].buildingName}</h4>
-                <label>adress</label>
-                <p className="desc">{blog["data"].adress}</p>
-                <label>price</label>
-                <span className="username">
-                  Created by <b>{blog["data"].price}</b>
-                </span>
-              </div>
+              <Card className={classes.root}>
+                <CardContent>
+                  <Typography className={classes.title} color="textSecondary" gutterBottom>
+                    {blog["data"].price}
+                  </Typography>
+                  <Typography variant="h5" component="h2">
+                     {blog["data"].buildingName} DT
+                  </Typography>
+                  <Typography className={classes.pos} color="textSecondary">
+                  {blog["data"].telephone}
+                  </Typography>
+                  <Typography variant="body2" component="p">
+                    {blog["data"].adress}
+                    <br />
+                    {blog["data"].zipcode}
+                  </Typography>
+                </CardContent>
+              </Card>
             </Popup>
-            ) : (<></>)}
-          
-
-       
-
-      
-      </Marker>))}
+          ) : (<></>)}
+        </>
+      ))}
 
     </ReactMapGL>
   );
