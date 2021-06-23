44f1ceb050f7dd91bc34cebac7d4546cfb88ebf1
atnomoverflow <76430267+atnomoverflow@users.noreply.github.com>
2021-06-23 15:13:45 +0200

    notification of message like fb

---
+++ samsara_v1/src/Profile/Pages/Message.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+
+
+
+const useStyles = makeStyles((theme) => ({
+  
+}));
+
+
+export default function Dashboard() {
+  const classes = useStyles();
+  return (
+    <>
+      
+    </>
+  );
+}
--- samsara_v1/src/Profile/Pages/Profile.js
+++ samsara_v1/src/Profile/Pages/Profile.js
@@ -1,98 +1,26 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import {
-  CssBaseline,
   Box,
   Container,
   Grid,
 } from "@material-ui/core";
-
-
 import AccountProfile from '../../Profile/account/AccountProfile';
 import AccountProfileDetails from '../../Profile/account/AccountProfileDetails';
-
-
-
-
-const drawerWidth = 240;
-
 const useStyles = makeStyles((theme) => ({
-  root: {
-    display: "flex",
-  },
-  toolbar: {
-    paddingRight: 24, // keep right padding when drawer closed
-  },
-  toolbarIcon: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "flex-end",
-    padding: "0 8px",
-    ...theme.mixins.toolbar,
-  },
-  appBar: {
-    zIndex: theme.zIndex.drawer + 1,
-    transition: theme.transitions.create(["width", "margin"], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-  },
-  appBarShift: {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(["width", "margin"], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  menuButton: {
-    marginRight: 36,
-  },
-  menuButtonHidden: {
-    display: "none",
-  },
-  title: {
-    flexGrow: 1,
-  },
-  drawerPaper: {
-    position: "relative",
-    whiteSpace: "nowrap",
-    width: drawerWidth,
-    transition: theme.transitions.create("width", {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  drawerPaperClose: {
-    overflowX: "hidden",
-    transition: theme.transitions.create("width", {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    width: theme.spacing(7),
-    [theme.breakpoints.up("sm")]: {
-      width: theme.spacing(9),
-    },
-  },
-  appBarSpacer: theme.mixins.toolbar,
-  content: {
-    flexGrow: 1,
-    height: "100vh",
-    overflow: "auto",
-  },
+  
   container: {
     paddingTop: theme.spacing(4),
     paddingBottom: theme.spacing(4),
   },
-  paper: {
-    padding: theme.spacing(2),
-    display: "flex",
-    overflow: "auto",
-    flexDirection: "column",
-  },
-  fixedHeight: {
-    height: 240,
-  },
+ flexIt:{
+  display: "flex"
+ },
+ boxIt:{
+  backgroundColor: "background.default",
+  minHeight: "100%",
+  py: 3,
+ }
 }));
 
 export default function Dashboard() {
@@ -100,19 +28,13 @@ export default function Dashboard() {
   const classes = useStyles();
   return (
     <>
-    
-        <div className={classes.appBarSpacer} />
         <Container maxWidth="lg" className={classes.container}>
           <Box
-            style={{
-              backgroundColor: "background.default",
-              minHeight: "100%",
-              py: 3,
-            }}
+            className={classes.boxIt}
           >
             <Container maxWidth="lg">
               <Grid container spacing={3}>
-                <Grid style={{display: "flex"}} item lg={4} md={6} xs={12}>
+                <Grid className={classes.flexIt} item lg={4} md={6} xs={12}>
                   <AccountProfile />
                 </Grid>
                 <Grid  item lg={8} md={6} xs={12}>
