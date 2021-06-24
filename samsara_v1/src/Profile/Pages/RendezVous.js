28e75c3755adc7151b052fa6d8f6a4c1a824110c
soufiane <soufianebenyaala1998@gmail.com>
2021-06-24 02:54:55 +0100

    rendez vous

--- samsara_v1/src/Profile/Pages/RendezVous.js
+++ samsara_v1/src/Profile/Pages/RendezVous.js
@@ -1,502 +1,81 @@
-import React from "react";
-import PropTypes from "prop-types";
-import clsx from "clsx";
-import { lighten, makeStyles } from "@material-ui/core/styles";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
-import TableHead from "@material-ui/core/TableHead";
-import TablePagination from "@material-ui/core/TablePagination";
-import TableRow from "@material-ui/core/TableRow";
-import TableSortLabel from "@material-ui/core/TableSortLabel";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import Paper from "@material-ui/core/Paper";
-import Checkbox from "@material-ui/core/Checkbox";
-import IconButton from "@material-ui/core/IconButton";
-import Tooltip from "@material-ui/core/Tooltip";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Switch from "@material-ui/core/Switch";
-import DeleteIcon from "@material-ui/icons/Delete";
-import FilterListIcon from "@material-ui/icons/FilterList";
+import React, { useState, useEffect } from "react";
+
+import { makeStyles } from "@material-ui/core/styles";
+import Fab from "@material-ui/core/Fab";
+import AddIcon from "@material-ui/icons/Add";
+
+import { Container } from "@material-ui/core";
+
+import RendezVousTable from "../RendezVousTable/RendezVousTable";
+import { useAuth } from "../../contexts/AuthContext";
 import { db } from "../../firebase";
-import { useState, useEffect } from "react";
-import ShowMoreText from "react-show-more-text";
-import ExpandLess from "@material-ui/icons/ExpandLess";
-import ExpandMore from "@material-ui/icons/ExpandMore";
-import EditIcon from '@material-ui/icons/Edit';
-function createData(
-  Building_name,
-  address,
-  zip_code,
-  price,
-  NumberOfRooms,
-  NumberOfBathRooms,
-  categorie,
-  discerption
-) {
-  return {
-    Building_name,
-    address,
-    zip_code,
-    price,
-    NumberOfRooms,
-    NumberOfBathRooms,
-    categorie,
-    discerption,
-  };
-}
-var rows = [];
+import { useHistory } from "react-router-dom";
 
-function addDataTorows(blogs) {
-  /*for(var i = 0 ; i<blogs.length;i++){
-    rows[i]={Building_name:blogs[i].buildingName,address:blogs[i].adress ,zip_code:blogs[i].zipcode , price:blogs[i].price,NumberOfRooms:blogs[i].NumberOfRooms ,NumberOfBathRooms:blogs[i].NumberOfBathRooms ,categorie:blogs[i].categorie ,discerption:blogs[i].discerption}
-  }*/
-  blogs &&
-    blogs.map((blog) => {
-      rows.push({
-        id_Building:blog.id,
-        Building_name: blog["data"].buildingName,
-        address: blog["data"].adress,
-        zip_code: blog["data"].zipcode,
-        price: blog["data"].price,
-        NumberOfRooms: blog["data"].NumberOfRooms,
-        NumberOfBathRooms: blog["data"].NumberOfBathRooms,
-        categorie: blog["data"].categorie,
-        discerption: blog["data"].discerption,
-      });
-    });
-}
-
-
-function descendingComparator(a, b, orderBy) {
-  if (b[orderBy] < a[orderBy]) {
-    return -1;
-  }
-  if (b[orderBy] > a[orderBy]) {
-    return 1;
-  }
-  return 0;
-}
-
-function getComparator(order, orderBy) {
-  return order === "desc"
-    ? (a, b) => descendingComparator(a, b, orderBy)
-    : (a, b) => -descendingComparator(a, b, orderBy);
-}
-
-function stableSort(array, comparator) {
-  const stabilizedThis = array.map((el, index) => [el, index]);
-  stabilizedThis.sort((a, b) => {
-    const order = comparator(a[0], b[0]);
-    if (order !== 0) return order;
-    return a[1] - b[1];
-  });
-  return stabilizedThis.map((el) => el[0]);
-}
-
-const headCells = [
-  {
-    id: "Building_name",
-    numeric: true,
-    disablePadding: true,
-    label: "Building name",
-  },
-  { id: "address", numeric: false, disablePadding: true, label: "address" },
-  { id: "zip_code", numeric: false, disablePadding: true, label: "zip code" },
-  { id: "price", numeric: false, disablePadding: true, label: "price" },
-  {
-    id: "NumberOfRooms",
-    numeric: true,
-    disablePadding: true,
-    label: "NumberOfRooms",
-  },
-  {
-    id: "NumberOfBathRooms",
-    numeric: true,
-    disablePadding: true,
-    label: "NumberOfBathRooms",
-  },
-  { id: "categorie", numeric: true, disablePadding: true, label: "categorie" },
-  {
-    id: "discerption",
-    numeric: true,
-    disablePadding: true,
-    label: "discerption",
-  },
-  {
-    id: "edit",
-    numeric: true,
-    disablePadding: true,
-    label: "Edit",
-  },
-  {
-    id: "delete",
-    numeric: true,
-    disablePadding: true,
-    label: "Delete",
-  },
-];
-
-function EnhancedTableHead(props) {
-  const {
-    classes,
-    onSelectAllClick,
-    order,
-    orderBy,
-    numSelected,
-    rowCount,
-    onRequestSort,
-  } = props;
-  const createSortHandler = (property) => (event) => {
-    onRequestSort(event, property);
-  };
-
-  return (
-    <TableHead style={{background: "cornflowerblue"}}>
-      <TableRow>
-        <TableCell padding="checkbox">
-          <Checkbox
-            indeterminate={numSelected > 0 && numSelected < rowCount}
-            checked={rowCount > 0 && numSelected === rowCount}
-            onChange={onSelectAllClick}
-            inputProps={{ "aria-label": "select all desserts" }}
-          />
-        </TableCell>
-        {headCells.map((headCell) => (
-          <TableCell
-            key={headCell.id}
-            align={headCell.numeric ? "center" : "left"}
-            padding={headCell.disablePadding ? "none" : "default"}
-            sortDirection={orderBy === headCell.id ? order : false}
-            style={{color:"#fff"}}
-          >
-            <TableSortLabel
-              active={orderBy === headCell.id}
-              direction={orderBy === headCell.id ? order : "asc"}
-              onClick={createSortHandler(headCell.id)}
-            >
-              {headCell.label}
-              {orderBy === headCell.id ? (
-                <span className={classes.visuallyHidden}>
-                  {order === "desc" ? "sorted descending" : "sorted ascending"}
-                </span>
-              ) : null}
-            </TableSortLabel>
-          </TableCell>
-        ))}
-      </TableRow>
-    </TableHead>
-  );
-}
-
-EnhancedTableHead.propTypes = {
-  classes: PropTypes.object.isRequired,
-  numSelected: PropTypes.number.isRequired,
-  onRequestSort: PropTypes.func.isRequired,
-  onSelectAllClick: PropTypes.func.isRequired,
-  order: PropTypes.oneOf(["asc", "desc"]).isRequired,
-  orderBy: PropTypes.string.isRequired,
-  rowCount: PropTypes.number.isRequired,
-};
-
-const useToolbarStyles = makeStyles((theme) => ({
-  root: {
-    paddingLeft: theme.spacing(2),
-    paddingRight: theme.spacing(1),
-  },
-  highlight:
-    theme.palette.type === "light"
-      ? {
-          color: theme.palette.secondary.main,
-          backgroundColor: lighten(theme.palette.secondary.light, 0.85),
-        }
-      : {
-          color: theme.palette.text.primary,
-          backgroundColor: theme.palette.secondary.dark,
-        },
-  title: {
-    flex: "1 1 100%",
-  },
-}));
-
-const EnhancedTableToolbar = (props) => {
-  const classes = useToolbarStyles();
-  const { numSelected } = props;
-
-  return (
-    <Toolbar
-    style={{background: "cornflowerblue",color:"#fff"}}
-      className={clsx(classes.root, {
-        [classes.highlight]: numSelected > 0,
-      })}
-    >
-      {numSelected > 0 ? (
-        <Typography
-          className={classes.title}
-          color="inherit"
-          variant="subtitle1"
-          component="div"
-        >
-          {numSelected} selected
-        </Typography>
-      ) : (
-        <Typography
-          className={classes.title}
-          variant="h6"
-          id="tableTitle"
-          component="div"
-        >
-          Mes Proprites
-        </Typography>
-      )}
-
-      {numSelected > 0 ? (
-        <Tooltip title="Delete">
-          <IconButton aria-label="delete">
-            <DeleteIcon />
-          </IconButton>
-        </Tooltip>
-      ) : (
-        <Tooltip title="Filter list">
-          <IconButton aria-label="filter list">
-            <FilterListIcon />
-          </IconButton>
-        </Tooltip>
-      )}
-    </Toolbar>
-  );
-};
-
-EnhancedTableToolbar.propTypes = {
-  numSelected: PropTypes.number.isRequired,
-};
+import { Link } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    width: "100%",
+  container: {
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4),
   },
-  paper: {
-    width: "100%",
-    marginBottom: theme.spacing(2),
-  },
-  table: {
-    minWidth: 750,
-  },
-  visuallyHidden: {
-    border: 0,
-    clip: "rect(0 0 0 0)",
-    height: 1,
-    margin: -1,
-    overflow: "hidden",
-    padding: 0,
+  fab: {
     position: "absolute",
-    top: 20,
-    width: 1,
+    bottom: theme.spacing(6),
+    right: theme.spacing(6),
+    backgroundColor: "cornflowerblue",
+    color: "#fff",
+  },
+  noLinks: {
+    textDecoration: "none",
+    color: "black",
+    "&:hover": {
+      textDecoration: "none",
+      color: "black",
+    },
   },
 }));
 
-export default function RendeZVous() {
+export default function RendezVous() {
   const classes = useStyles();
-  const [order, setOrder] = React.useState("asc");
-  const [orderBy, setOrderBy] = React.useState("calories");
-  const [selected, setSelected] = React.useState([]);
-  const [page, setPage] = React.useState(0);
-  const [dense, setDense] = React.useState(false);
-  const [rowsPerPage, setRowsPerPage] = React.useState(5);
-
-  const [blogs, setBlogs] = useState([]);
-  const fetchBlogs = async () => {
-    const response =  db.collection("Allproduct");
-    const data = await response.get();
-
-    data.docs.map((item) => {
-       setBlogs((blogs) => [...blogs, {id:item.id,data:item.data()}]);
-    });
-  };
+  const history = useHistory();
+  const { currentUser } = useAuth();
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState([]);
 
   useEffect(() => {
-    fetchBlogs();
-  }, []);
-  addDataTorows(blogs);
-  console.log(rows)
+    const getPostsFromFirebase = [];
+    const subscriber = db
+      .collection("users")
+      .doc(currentUser.uid)
+      .collection("ReceiveRequestTour")
+      .onSnapshot((querySnaphot) => {
+        querySnaphot.forEach((doc) => {
+          getPostsFromFirebase.push({
+            ...doc.data(),
+            key: doc.id,
+          });
+        });
+        setPosts(getPostsFromFirebase);
+        setLoading(false);
+      });
 
-  const handleRequestSort = (event, property) => {
-    const isAsc = orderBy === property && order === "asc";
-    setOrder(isAsc ? "desc" : "asc");
-    setOrderBy(property);
-  };
-
-  const handleSelectAllClick = (event) => {
-    if (event.target.checked) {
-      const newSelecteds = rows.map((n) => n.Building_name);
-      setSelected(newSelecteds);
-      return;
-    }
-    setSelected([]);
-  };
-
-  const handleClick = (event, name) => {
-    const selectedIndex = selected.indexOf(name);
-    let newSelected = [];
-
-    if (selectedIndex === -1) {
-      newSelected = newSelected.concat(selected, name);
-    } else if (selectedIndex === 0) {
-      newSelected = newSelected.concat(selected.slice(1));
-    } else if (selectedIndex === selected.length - 1) {
-      newSelected = newSelected.concat(selected.slice(0, -1));
-    } else if (selectedIndex > 0) {
-      newSelected = newSelected.concat(
-        selected.slice(0, selectedIndex),
-        selected.slice(selectedIndex + 1)
-      );
-    }
-
-    setSelected(newSelected);
-  };
-  const [expand, setExpand] = useState(false);
-  const onClick = () => {
-    setExpand(!expand);
-  };
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (event) => {
-    setRowsPerPage(parseInt(event.target.value, 10));
-    setPage(0);
-  };
-
-  const handleChangeDense = (event) => {
-    setDense(event.target.checked);
-  };
-
-  const isSelected = (name) => selected.indexOf(name) !== -1;
-
-  const emptyRows =
-    rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
-
+    return () => {
+      subscriber();
+    };
+  }, [loading]);
+console.log(posts)
   return (
-    <div className={classes.root}>
-      <Paper className={classes.paper}>
-        <EnhancedTableToolbar numSelected={selected.length} />
-        <TableContainer>
-          <Table
-            className={classes.table}
-            aria-labelledby="tableTitle"
-            size={dense ? "small" : "medium"}
-            aria-label="enhanced table"
-          >
-            <EnhancedTableHead
-              classes={classes}
-              numSelected={selected.length}
-              order={order}
-              orderBy={orderBy}
-              onSelectAllClick={handleSelectAllClick}
-              onRequestSort={handleRequestSort}
-              rowCount={rows.length}
-            />
-            <TableBody>
-              {stableSort(rows, getComparator(order, orderBy))
-                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                .map((row, index) => {
-                  const isItemSelected = isSelected(row.Building_name);
-                  const labelId = `enhanced-table-checkbox-${index}`;
+    <>
+      <Container maxWidth="lg" className={classes.container}>
+        <Link className={classes.noLinks} to="/profile/AddProduct">
+          <Fab className={classes.fab} variant="extended">
+            <AddIcon />
+            ajouter maison
+          </Fab>
+        </Link>
 
-                  return (
-                    <TableRow
-                      hover
-                      onClick={(event) => handleClick(event, row.Building_name)}
-                      role="checkbox"
-                      aria-checked={isItemSelected}
-                      tabIndex={-1}
-                      key={row.id_Building}
-                      selected={isItemSelected}
-                    >
-                      <TableCell padding="checkbox">
-                        <Checkbox
-                          checked={isItemSelected}
-                          inputProps={{ "aria-labelledby": labelId }}
-                        />
-                      </TableCell>
-                      <TableCell
-                        component="th"
-                        id={labelId}
-                        scope="row"
-                        padding="none"
-                      >
-                        {row.Building_name}
-                      </TableCell>
-                      <TableCell align="center">{row.address}</TableCell>
-                      <TableCell align="center">{row.zip_code}</TableCell>
-                      <TableCell align="center">{row.price}</TableCell>
-                      <TableCell align="center">{row.NumberOfRooms}</TableCell>
-                      <TableCell align="center">
-                        {row.NumberOfBathRooms}
-                      </TableCell>
-                      <TableCell align="center">{row.categorie}</TableCell>
-                      <TableCell align="left">
-                        <ShowMoreText
-                          lines={2}
-                          more={<ExpandMore />}
-                          less={<ExpandLess />}
-                          onClick={onClick}
-                          expanded={expand}
-                          width={100}
-                        >
-                          {row.discerption}
-                        </ShowMoreText>
-                      </TableCell>
-                      <TableCell align="left">
-                        <IconButton
-                          aria-label="delete"
-                          disabled
-                          color="primary"
-                        >
-                          <EditIcon />
-                        </IconButton>
-                      </TableCell>
-                      <TableCell align="left">
-                        <IconButton
-                          aria-label="delete"
-                          disabled
-                          color="primary"
-                        >
-                          <DeleteIcon />
-                        </IconButton>
-                      </TableCell>
-                     
-                    </TableRow>
-                  );
-                })}
-              {emptyRows > 0 && (
-                <TableRow style={{ height: (dense ? 33 : 53) * emptyRows }}>
-                  <TableCell colSpan={6} />
-                </TableRow>
-              )}
-            </TableBody>
-          </Table>
-        </TableContainer>
-        <TablePagination
-          rowsPerPageOptions={[5, 10, 25]}
-          component="div"
-          count={rows.length}
-          rowsPerPage={rowsPerPage}
-          page={page}
-          onChangePage={handleChangePage}
-          onChangeRowsPerPage={handleChangeRowsPerPage}
-        />
-      </Paper>
-      <FormControlLabel
-        control={<Switch checked={dense} onChange={handleChangeDense} />}
-        label="Dense padding"
-      />
-    </div>
+        <RendezVousTable posts={posts} />
+      </Container>
+    </>
   );
 }
-
-
-
-
