da3d890a1f0444fc0d970baf6300084f850fe8b0
atnomoverflow <76430267+atnomoverflow@users.noreply.github.com>
2021-06-24 04:36:37 +0200

    aa

--- samsara_v1/src/navbar/SearchBar.js
+++ samsara_v1/src/navbar/SearchBar.js
@@ -1,68 +1,71 @@
-import React from 'react'
-import { fade,makeStyles } from '@material-ui/core/styles';
-import SearchIcon from '@material-ui/icons/Search';
-import InputBase from '@material-ui/core/InputBase';
+import React from "react";
+import { fade, makeStyles } from "@material-ui/core/styles";
+import SearchIcon from "@material-ui/icons/Search";
+import InputBase from "@material-ui/core/InputBase";
 const useStyles = makeStyles((theme) => ({
-    search: {
-      position: 'relative',
-      borderRadius: theme.shape.borderRadius,
-      backgroundColor: fade(theme.palette.common.white, 0.15),
-      border: '1px solid #d5d9dc',
-      '&:hover': {
-        backgroundColor: fade(theme.palette.common.white, 0.25),
-      },
-      marginRight: theme.spacing(2),
-      marginLeft: 0,
-      width: '100%',
-      [theme.breakpoints.up('sm')]: {
-        marginLeft: theme.spacing(3),
-        width: 'auto',
-      },
+  search: {
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    border: "1px solid #d5d9dc",
+    "&:hover": {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
     },
-    searchIcon: {
-      padding: theme.spacing(0, 2),
-      height: '100%',
-      position: 'absolute',
-      pointerEvents: 'none',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(3),
+      width: "auto",
     },
-    inputRoot: {
-      color: 'inherit',
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputRoot: {
+    color: "inherit",
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    color: "black",
+    [theme.breakpoints.up("md")]: {
+      width: "20ch",
     },
-    inputInput: {
-      padding: theme.spacing(1, 1, 1, 0),
-      // vertical padding + font size from searchIcon
-      paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-      transition: theme.transitions.create('width'),
-      width: '100%',
-      color: "black",
-      [theme.breakpoints.up('md')]: {
-        width: '20ch',
-      },
-    },
-  }));
-  
-const SearchBar = (props) => {
-  const setSearch=props.setSearch;
-    const classes = useStyles();
-    return (
-        <div className={classes.search}>
-        <div className={classes.searchIcon}>
-          <SearchIcon style={{color:"cornflowerblue"}} />
-        </div>
-        <InputBase
-          placeholder="Search…"
-          classes={{
-            root: classes.inputRoot,
-            input: classes.inputInput,
-          }}
-          inputProps={{ 'aria-label': 'search' }}
-          onChange={event =>{setSearch(event.target.value);}}
-        />
-        </div>
-    )
-}
+  },
+}));
 
-export default SearchBar
+const SearchBar = (props) => {
+  const setSearch = props.setSearch;
+  const classes = useStyles();
+  return (
+    <div className={classes.search}>
+      <div className={classes.searchIcon}>
+        <SearchIcon style={{ color: "cornflowerblue" }} />
+      </div>
+      <InputBase
+        placeholder="Search…"
+        value={props.productid}
+        classes={{
+          root: classes.inputRoot,
+          input: classes.inputInput,
+        }}
+        inputProps={{ "aria-label": "search" }}
+        onChange={(event) => {
+          setSearch(event.target.value);
+        }}
+      />
+    </div>
+  );
+};
+
+export default SearchBar;
